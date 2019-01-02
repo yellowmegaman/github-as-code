6670b2674d8cb51dbbbd84110b2959374b84dc68
emik0 <yellowmegaman@gmail.com>
2019-01-03 01:17:25 +0300

    org nam

--- github.tf
+++ github.tf
@@ -3,11 +3,13 @@ provider "github" {
 }
 
 resource "github_repository" "docker-packer" {
-  name        = "docker-packer"
-  description = "Docker stable + Hashicorp packer"
+  name         = "docker-packer"
+  organization = "sullenbode"
+  description  = "Docker stable + Hashicorp packer"
 }
 
 resource "github_repository" "docker-bastion" {
-  name        = "docker-bastion"
-  description = "Alpine stable + glider + openssh"
+  name         = "docker-bastion"
+  organization = "sullenbode"
+  description  = "Alpine stable + glider + openssh"
 }
