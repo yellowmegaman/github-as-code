f1553bfb6217f119ecbc5eeccaa736045b16be6f
emik0 <yellowmegaman@gmail.com>
2019-01-03 01:18:52 +0300

    org name

--- github.tf
+++ github.tf
@@ -1,15 +1,14 @@
 provider "github" {
   token        = "${var.github_token}"
+  organization = "sullenbode"
 }
 
 resource "github_repository" "docker-packer" {
   name         = "docker-packer"
-  organization = "sullenbode"
   description  = "Docker stable + Hashicorp packer"
 }
 
 resource "github_repository" "docker-bastion" {
   name         = "docker-bastion"
-  organization = "sullenbode"
   description  = "Alpine stable + glider + openssh"
 }
