f7d1ffb474d0a154a276098434e17b13f1ed7712
emik0 <yellowmegaman@gmail.com>
2019-01-03 23:20:41 +0300

    I'll wait till personal repos supported

--- github.tf
+++ github.tf
@@ -4,13 +4,13 @@ provider "github" {
 }
 
 resource "github_repository" "docker-packer" {
-  count        = 1
+  count        = 0
   name         = "docker-packer"
   description  = "Docker stable + Hashicorp packer"
 }
 
 resource "github_repository" "docker-bastion" {
-  count        = 1
+  count        = 0
   name         = "docker-bastion"
   description  = "Alpine stable + glider + openssh"
 }
